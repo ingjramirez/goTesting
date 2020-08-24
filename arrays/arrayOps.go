d480c4541155db0cf572177a7451a207acc731b4
Jaime Ramirez <jaimefrancisco.ramirezolmos@sony.com>
2020-08-24 16:37:33 +0200

    Add structs

--- arrays/arrayOps.go
+++ arrays/arrayOps.go
@@ -1,11 +1,19 @@
 package main
 
 func Sum(numbers []int) int {
-
 	var sum int
 	for _, number := range numbers {
 		sum += number
 	}
 
 	return sum
+}
+
+func SumAll(arrays ...[]int) []int {
+	var results []int
+	for _, array := range arrays {
+		results = append(results, Sum(array))
+	}
+
+	return results
 }
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,11 +1,14 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func TestSum(t *testing.T)  {
+func TestSum(t *testing.T) {
 
 	t.Run("collection of 5 numbers", func(t *testing.T) {
-		numbers := [] int {1, 2, 3, 4, 5}
+		numbers := []int{1, 2, 3, 4, 5}
 
 		got := Sum(numbers)
 		want := 15
@@ -23,6 +26,36 @@ func TestSum(t *testing.T)  {
 
 		if got != want {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+
+	t.Run("aggregate multiple arrays", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	t.Run("aggregate single array", func(t *testing.T) {
+		got := SumAll([]int{1, 1, 1})
+		want := []int{3}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	t.Run("no args produces empty array", func(t *testing.T) {
+		got := SumAll()
+		var want []int
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
 		}
 	})
 }
